070f422cac142a128481709ecc8190d38cd1c78d
AbigailKemuma1 <abigail.kemuma@student.moringaschool.com>
2025-09-25 11:52:02 +0300

    Update models.py

--- flask-server/models.py
+++ flask-server/models.py
@@ -1,4 +1,4 @@
-from extensions import db, ma
+from extensions import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,8 +8,12 @@ class FoodListing(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    stock = db.Column(db.Integer, nullable=False, default=1)
+    price = db.Column(db.Float, nullable=False, default=0.0)
+    expiry_date = db.Column(db.Date, nullable=True)  # optional, can validate in route
 
 class Purchase(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     food_id = db.Column(db.Integer, db.ForeignKey('food_listing.id'))
+    quantity_bought = db.Column(db.Integer, nullable=False, default=1)
